b17de0513f5f1831a24b13cf014efbbfb6575d4a
Mostafa Tarek <mostafa.tarek.abdeljawad@gmail.com>
2026-09-02 13:55:43 +0300

    added interceptors to the requests to attach jwt

---
+++ authlab-client/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,22 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+
+
+// interceptor automatically attaching the token to every reqest
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const authService = inject(AuthService);
+  const token = authService.getToken();
+
+  if(token){
+    const clonedReq = req.clone({
+      setHeaders:{
+        Authorization:`Bearer ${token}`
+      }
+    });
+    return next(clonedReq);
+  }
+
+  return next(req);
+};
--- authlab-client/src/app/services/auth.service.ts
+++ authlab-client/src/app/services/auth.service.ts
@@ -3,6 +3,8 @@ import { RegisterDto } from '../dtos/RegisterDto';
 import { UserResponseDto } from '../dtos/UserResponseDto';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { LoginDto } from '../dtos/loginDto';
+import { LoginResponse } from '../dtos/loginResponseDto';
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +18,25 @@ export class AuthService {
   register(dto: RegisterDto): Observable<UserResponseDto>{
     return this.http.post<UserResponseDto>(`${this.baseUrl}/register`, dto);
   }
+
+  login(dto: LoginDto): Observable<LoginResponse>{
+    return this.http.post<LoginResponse>(`${this.baseUrl}/login`,dto);
+  }
+
+  saveToken(token:string): void{
+    localStorage.setItem('accessToken',token);
+  }
+
+  getToken(): string|null{
+    return localStorage.getItem('accessToken');
+  }
+
+  logout(): void{
+    localStorage.removeItem('accessToken');
+  }
+
+  isLoggedIn(): boolean{
+    return !!this.getToken;
+  }
+  
 }
