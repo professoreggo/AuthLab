cd7373baa93a052c4a7cbbff321f80b867af8225
Mostafa Tarek <mostafa.tarek.abdeljawad@gmail.com>
2026-09-02 14:48:15 +0300

    added auth gaurd and applied on usersComponent

--- authlab-client/src/app/app.routes.ts
+++ authlab-client/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserslistComponent } from './components/userslist/userslist.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'users' , component: UserslistComponent}
+    { path: 'users', component: UserslistComponent, canActivate: [authGuard] }
 ];
