13de3994ea5c4c66f9f9024c27da6ef979211664
Mostafa Tarek <mostafa.tarek.abdeljawad@gmail.com>
2026-09-02 22:34:29 +0300

    added interceptor handling to unauthorized 401 and rerouting to login page

--- authlab-client/src/app/interceptors/auth.interceptor.ts
+++ authlab-client/src/app/interceptors/auth.interceptor.ts
@@ -1,22 +1,29 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 
 // interceptor automatically attaching the token to every reqest
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
   const authService = inject(AuthService);
+  const router = inject(Router);
   const token = authService.getToken();
 
-  if(token){
-    const clonedReq = req.clone({
-      setHeaders:{
-        Authorization:`Bearer ${token}`
-      }
-    });
-    return next(clonedReq);
-  }
+  const clonedRequest = token
+    ? req.clone({setHeaders:{Authorization: `Bearer ${token}`}})
+    :req;
 
-  return next(req);
+  return next(clonedRequest).pipe(
+    catchError((error)=>{
+      if ( error.status === 401){  // catch if unauthorized then delete the invalide token then naviagte to '/login'
+        authService.logout();
+        router.navigate(['/login']);
+
+      }
+      return throwError(()=>error);
+    })
+  );
 };
